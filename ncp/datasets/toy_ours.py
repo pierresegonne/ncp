420d5b8022162fdf920012daf32e5f49038f75c9
Pierre Segonne <pierre.segonne@student.ecp.fr>
2021-09-27 21:59:51 +0200

    Fix domain for toy_ours

--- ncp/datasets/toy_ours.py
+++ ncp/datasets/toy_ours.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from ncp import tools
 
 
@@ -13,9 +14,7 @@ def generate_toy_ours_dataset():
 
     training_range = [0, 10]
     testing_range = [0, 10]
-    # Ours was
-    # testing_range = [-0.5, 10.5]
-    N_train = 3
+    N_train = 10
     N_test = 100
     x_train = np.random.uniform(
         low=training_range[0], high=training_range[1], size=N_train
@@ -31,11 +30,12 @@ def generate_toy_ours_dataset():
     x_train, y_train = (x_train - x_mu) / x_sigma, (y_train - y_mu) / y_sigma
 
     training_range = [x_train.min(), x_train.max()]
+    domain = np.linspace(training_range[0], training_range[1], 1000)
 
     train = tools.AttrDict(inputs=x_train[:, None], targets=y_train[:, None])
     test = tools.AttrDict(inputs=x_test[:, None], targets=y_test[:, None])
     return tools.AttrDict(
-        domain=np.array(training_range)[:, None], train=train, test=test, target_scale=1
+        domain=domain[:, None], train=train, test=test, target_scale=1
     )
 
 
